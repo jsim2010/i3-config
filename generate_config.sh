807db6186183e880bb0ffd76715e1182a17e78c2
Justin Simerly <justinsimerly1992@gmail.com>
2016-10-20 19:06:13 -0500

    Removing generate_config.sh file from root directory

--- generate_config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-LOCAL_CONFIG=$HOME/.config/i3/config.local
-
-if [ ! -f $LOCAL_CONFIG ]; then
-    touch $LOCAL_CONFIG
-fi
-
-cat $LOCAL_CONFIG $HOME/.config/i3/config.base > $HOME/.config/i3/config
